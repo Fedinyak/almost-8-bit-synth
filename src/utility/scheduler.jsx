a3459ac627f3258dc104269562bb2d6357b6bf30
Andrey Fedinyak <fedinyakandrey@gmail.com>
2026-02-16 20:58:26 +0300

    Fix sequencer

--- src/utility/scheduler.jsx
+++ src/utility/scheduler.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useRef } from "react";
 import * as Tone from "tone";
-// import BpmVisualizer from "../components/sequencer/BpmVisualizer";
-// import { synth } from "./playSound";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentStep } from "../slices/sequencerSlice";
 
@@ -18,27 +16,7 @@ const TimerTransport = () => {
 
   // 1. Make container. Don't change on render
   const enginesRef = useRef({});
-  // const partRef = useRef({});
   const partsRef = useRef({});
-
-  // // 2. Initialization on first start app
-  // useEffect(() => {
-  //   // Save a link to it in partRef.current
-  //   partRef.current = new Tone.Part((time, event) => {
-  //     synth.triggerAttackRelease(event.note, event.duration, time);
-  //   }, []).start(0);
-
-  //   partRef.current.loop = true;
-  //   // partRef.current.loopEnd = "2m"; // loop length
-  //   partRef.current.loopEnd = "1m"; // loop length
-
-  //   // If component deleted, clear sound from memory
-  //   return () => {
-  //     if (partRef.current) {
-  //       partRef.current.dispose();
-  //     }
-  //   };
-  // }, []);
 
   useEffect(() => {
     instrumentsList.forEach(instrument => {
@@ -66,23 +44,19 @@ const TimerTransport = () => {
           }).toDestination();
         }
 
-        // enginesRef.current[instrument] = { kick, snare }; // Сохраняем пачкой
         const currentId = instrument;
         const currentType = data.type;
 
         // Создаем партию с логикой распределения нот
         const newPart = new Tone.Part((time, value) => {
           const engine = enginesRef.current[currentId];
-          // if (!engine) return;
           if (currentType === "drums") {
-            // Если это барабаны, вызываем методы ВНУТРЕННИХ объектов (kick/snare)
             if (value.note === "C1") {
               engine.kick.triggerAttackRelease("C1", "8n", time);
             } else if (value.note === "D1") {
               engine.snare.triggerAttackRelease("16n", time);
             }
           } else {
-            // Если это синт, вызываем метод напрямую у engine
             engine.triggerAttackRelease(value.note, value.duration, time);
           }
         }, []).start(0);
@@ -90,27 +64,6 @@ const TimerTransport = () => {
         newPart.loopEnd = "1m";
 
         partsRef.current[instrument] = newPart;
-        // drumPart.loop = true;
-        // drumPart.loopEnd = "1m";
-        // partsRef.current[instrument] = drumPart;
-        // } else {
-        //   enginesRef.current[instrument] = new Tone.MonoSynth({
-        //     oscillator: { type: "square" },
-        //     envelope: { attack: 0.005, decay: 0.1, sustain: 0.3, release: 1 },
-        //   }).toDestination();
-
-        // ШАГ 2: Создание Партии (Tone.Part)
-        // Это планировщик, который будет вызывать звук.
-        // Передаем callback: "Когда наступит время 'time', возьми данные 'value' и играй"
-        // const newPart = new Tone.Part((time, value) => {
-        //   enginesRef.current[instrument].triggerAttackRelease(
-        //     value.note, // Какую ноту играть (например, "C4")
-        //     value.duration, // Как долго (например, "8n")
-        //     time, // Точное время из аудио-контекста
-        //   );
-        // }, []).start(0); // Запускаем партию сразу, чтобы она слушала транспорт
-
-        // --- ОПТИМИЗАЦИЯ: Настройка Loop только один раз при создании ---
       }
 
       // ШАГ 3: Синхронизация данных сетки с Tone.js
@@ -138,7 +91,6 @@ const TimerTransport = () => {
         // 1. Останавливаем и удаляем планировщик
         partsRef.current[item].dispose();
         // 2. Удаляем сам синтезатор из памяти аудио-контекста
-        // enginesRef.current[item].dispose();
         const engine = enginesRef.current[item];
         if (engine.kick && engine.snare) {
           // Если это барабаны (объект), удаляем оба узла
@@ -157,21 +109,6 @@ const TimerTransport = () => {
       enginesRef.current = {};
     };
   }, [instrumentsData, instrumentsList]);
-
-  // Change date on change grid
-  // useEffect(() => {
-  //   if (partRef.current) {
-  //     // Очищаем только события внутри ПАРТИИ, не удаляя саму партию
-  //     partRef.current.clear();
-
-  //     // Заполняем существующую партию новыми данными из пропса
-  //     sequencerNoteGrid.forEach(item => {
-  //       if (item.note) {
-  //         partRef.current.add(item.time, item);
-  //       }
-  //     });
-  //   }
-  // }, [sequencerNoteGrid]); // Следим за изменениями сетки
 
   useEffect(() => {
     if (sequencerPlayState === "start") {
