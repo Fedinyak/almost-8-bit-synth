dbc7f66721232a5a24f445147b45fe48d8dd6e27
Andrey Fedinyak <fedinyakandrey@gmail.com>
2026-05-13 16:54:03 +0300

    delete useless code

--- src/slices/store.js
+++ src/slices/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counterSlice";
 import noteSlice from "./noteSlice";
 import sequencerSlice from "./sequencerSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     note: noteSlice,
     sequencer: sequencerSlice,
   },
